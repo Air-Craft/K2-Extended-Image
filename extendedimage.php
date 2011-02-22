f073ee1c551eddd72416e5b5fd24ef48e0bbc858
Hari Karam Singh <harikaram@regallygraceful.com>
2011-02-22 16:16:25 +0000

    Fix image sizeinfo in category view to take into account context (primary, leading, etc).

--- extendedimage.php
+++ extendedimage.php
@@ -108,15 +108,34 @@ class plgK2ExtendedImage extends K2Plugin
 		if (@isset($item->k2ExtImageWidth) && ($item->k2ExtImageWidth == $item->imageWidth))
 			return;
 
-		$prop = 'image' . $item->params->get('itemImgSize');
+		// Get the image property name in the item object based on the item's group
+		switch (@$item->itemGroup)
+		{
+			case 'leading':
+			case 'primary':
+			case 'secondary':
+			case 'links':
+			case 'feed':
+				$prop = 'image' . $item->params->get($item->itemGroup. 'ImgSize');
+				break;
+			default:	// item view
+				if (JRequest::getCmd('view') == 'item')
+					$prop = 'image' . $item->params->get('itemImgSize');
+				else
+					return;	// can't handle generic now as there's no itemGroup for it!
+		}
+
 		$imageUrl = $item->$prop;
+		if (empty($imageUrl))
+			return;
+
 		$info = getimagesize(JPATH_SITE.DS.'media'.DS.'k2'.DS.'items'.DS.'cache'.DS.pathinfo($imageUrl, PATHINFO_BASENAME));
 
 		$item->imageWidth = $info[0];
 		$item->k2ExtImageWidth = $info[0];
 		$item->k2ExtImageHeight = $info[1];
 		$item->k2ExtImageAttr = $info[3];
-		fb($info);
+		
 	}
 
 	/**
